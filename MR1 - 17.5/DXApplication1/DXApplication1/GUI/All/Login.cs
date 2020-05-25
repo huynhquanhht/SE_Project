3135b1031ce08c20e2bc5105395251e8c8d0ca01
huynhquanhht <huynhquanhht@gmail.com>
2020-05-25 12:05:49 +0700

    Message

---
+++ MR1 - 17.5/DXApplication1/DXApplication1/GUI/All/Login.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DXApplication1
+{
+    public partial class Login : Form
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void labelControl2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void labelControl1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ MR1 - 17.5/DXApplication1/DXApplication1/GUI/TabSystem/Bill.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DevExpress.XtraEditors;
+
+namespace DXApplication1.GUI
+{
+    public partial class Bill : DevExpress.XtraEditors.XtraForm
+    {
+        public Bill()
+        {
+            InitializeComponent();
+        }
+
+        private void grd_Item_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void gridControl1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
